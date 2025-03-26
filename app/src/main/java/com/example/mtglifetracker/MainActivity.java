2a99ca4abac61107d9bf30324e0e95afa75d07ad
Vimor123 <vmladar1@gmail.com>
2025-03-26 15:02:59 +0100

    Camera cutout is covered :pray:

--- app/src/main/java/com/example/mtglifetracker/MainActivity.java
+++ app/src/main/java/com/example/mtglifetracker/MainActivity.java
@@ -23,8 +23,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+        getWindow().getDecorView().setSystemUiVisibility(flags);
+
         setContentView(R.layout.activity_main);
 
         player1Life = 20;
